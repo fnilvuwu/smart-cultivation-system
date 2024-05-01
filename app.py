44452db1f81ead2fb98e74dd36566877b8e269d3
fnilvuwu <mamatmks45@gmail.com>
2024-05-01 22:17:46 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -15,33 +15,29 @@ from rate_limiter import init_rate_limiter
 from database.mysql import init_db, init_db_command, drop_db_command
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.update(CONFIG)
+app = Flask(__name__)
+app.config.update(CONFIG)
 
-    # Register Blueprint
-    app.register_blueprint(index_bp)
-    app.register_blueprint(error_bp)
-    app.register_blueprint(llt_bp)
-    app.register_blueprint(rd_bp)
-    app.register_blueprint(db_bp)
+# Register Blueprint
+app.register_blueprint(index_bp)
+app.register_blueprint(error_bp)
+app.register_blueprint(llt_bp)
+app.register_blueprint(rd_bp)
+app.register_blueprint(db_bp)
 
-    # Initializing
-    init_cache_app(app)
-    init_compress_app(app)
-    init_rate_limiter(app)
-    init_db(app)
+# Initializing
+init_cache_app(app)
+init_compress_app(app)
+init_rate_limiter(app)
+init_db(app)
 
-    socketio.init_app(app, cors_allowed_origins="*")
+socketio.init_app(app, cors_allowed_origins="*")
 
-    # CLI Command
-    app.cli.add_command(init_db_command)
-    app.cli.add_command(drop_db_command)
-
-    return app
-
+# CLI Command
+app.cli.add_command(init_db_command)
+app.cli.add_command(drop_db_command)
 
 if __name__ == "__main__":
-    app = create_app()
-    socketio.run(app, debug=True, host="0.0.0.0",
-                 port=int(os.environ.get("PORT", 8080)))
+    socketio.run(
+        app, debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))
+    )
