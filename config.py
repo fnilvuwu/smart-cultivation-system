5dfc0c8f6dcbc5d86a7ee31088d9a2950e0bd03f
Rahmatullah. R <mamatmks45@gmail.com>
2024-07-03 22:52:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,15 +8,7 @@ CONFIG = {
     "CACHE_TYPE": "SimpleCache",
     "CACHE_DEFAULT_TIMEOUT": 300,
     "SESSION_TYPE": "filesystem",
-    "SQLALCHEMY_DATABASE_URI": (
-        "postgresql://"
-        + os.getenv("POSTGRES_USER")
-        + ":"
-        + os.getenv("POSTGRES_PASSWORD")
-        + "@"
-        + os.getenv("POSTGRES_HOST")
-        + "/"
-        + os.getenv("POSTGRES_DATABASE")
-    ),
+    "SQLALCHEMY_DATABASE_URI": os.getenv(
+    "POSTGRES_URL_NON_POOLING"),
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
 }
